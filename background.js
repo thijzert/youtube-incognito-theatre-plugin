24cf8f3432858992d1c35af29c7ea768113762b8
Thijs van Dijk <github@inurbanus.nl>
2016-05-31 22:53:15 +0200

    Convert to an event page

--- background.js
+++ background.js
@@ -6,47 +6,55 @@
  **/
 
 var YOUTUBE_DOMAIN = 'youtube.com';
-var YOUTUBE_URL = 'http://' + YOUTUBE_DOMAIN;
+var YOUTUBE_URL = 'https://www.youtube.com';
 
 /**
  * Set 'disable autoplay' and 'theatre mode' cookies
  *
  * @param {string} store_id The cookie store the cookie should be stored in.
  */
-function theatre(store_id) {
+function theatre(store_id)
+{
 	var to_set = [
-		{key: "wide", value: "1"},  // Theatre (or 'Cinema') mode
-		{key: "PREF", value: "f1=50000000&f5=30030&gl=US"},  // Disable autoplay, global mode
-		{key: "HideTicker", value: "true"}  // Remind me later
+		{name: "wide", value: "1"},  // Theatre (or 'Cinema') mode
+		{name: "PREF", value: "f1=50000000&f5=30030&gl=US"},  // Disable autoplay, global mode
+		{name: "HideTicker", value: "true"}  // Remind me later
 	];
 
 	for ( var i = 0; i < to_set.length; i++ )
 	{
-		chrome.cookies.get({
-				name: to_set[i].key,
-				url: YOUTUBE_URL,
-				storeId: store_id
-			},
-			function(cookie)
-			{
-				if (!cookie || cookie.value != to_set[i].value) {
-					chrome.cookies.set({
-						name: to_set[i].key,
-						value: to_set[i].value,
-						domain: '.' + YOUTUBE_DOMAIN,
-						url: YOUTUBE_URL,
-						expirationDate: 10*365*86400*1000 + (new Date()).getTime(),
-						storeId: store_id
-					});
-				}
-			});
+		chrome.cookies.set({
+			name: to_set[i].name,
+			value: to_set[i].value,
+			domain: '.' + YOUTUBE_DOMAIN,
+			url: YOUTUBE_URL,
+			secure: true,
+			expirationDate: 10*365*86400 + (new Date()).getTime() / 1000,
+			storeId: store_id
+		});
 	}
 }
 
-chrome.cookies.getAllCookieStores(function(cookie_stores)
+chrome.cookies.onChanged.addListener(function()
 {
-	for ( var i = 0; i < cookie_stores.length; i++ )
+	if ( evt.cookie.domain == '.' + YOUTUBE_DOMAIN )
 	{
-		theatre( cookie_stores[i].id );
+		theatre( evt.cookie.storeId );
 	}
 });
+
+
+function theatre_all()
+{
+	chrome.cookies.getAllCookieStores(function(cookie_stores)
+	{
+		for ( var i = 0; i < cookie_stores.length; i++ )
+		{
+			theatre( cookie_stores[i].id );
+		}
+	});
+}
+chrome.windows.onCreated.addListener(theatre_all);
+
+theatre_all();
+
